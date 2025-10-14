e2b4174d2b1a99b85b230242b0a53eaaf8ef8947
tianmeng <tian@me.ng>
2025-10-14 16:12:37 +0800

    项目文件

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,8 +5,8 @@ const router = createBrowserRouter(
   [
     { path: "/", element: <Home /> },
     { path: "*", element: <div>404</div> },
-  ]
-  // { basename: '/web-video-editor' }   // ← 前后都要带 /，不能多也不能少
+  ],
+  { basename: '/web-video-editor' }   // ← 前后都要带 /，不能多也不能少
 );
 
 export default router;
